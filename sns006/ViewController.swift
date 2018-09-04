a15f151fd4e555e88f45dd5a233c2f7fbf268c7d
fumufumu <tomohirosuzuki02@gmail.com>
2018-09-04 15:02:59 +0900

    test1

--- sns006/ViewController.swift
+++ sns006/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func showActivityView(_ sender: UIBarButtonItem) {
+      
+        let controller = UIActivityViewController(activityItems: [imageView.image!],applicationActivities: nil)
+        self.present(controller, animated: true, completion: {print("表示完了")})
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
